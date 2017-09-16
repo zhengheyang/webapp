22c36539d3e0c2206eb7e90e89ea31109b370f44
ROG\zhyang <zhenghe_yang@hotmail.com>
2017-09-16 23:31:16 +0800

    完成list数据绑定

--- src/scripts/controller/mainCtrl.js
+++ src/scripts/controller/mainCtrl.js
@@ -1,4 +1,18 @@
 'use strict';
-angular.module('app').controller('mainCtrl',['$scope',function () {
-
+angular.module('app').controller('mainCtrl',['$scope',function ($scope) {
+  $scope.list=[{
+    name:'销售',
+    imgSrc:'image/company-3.png',
+    companyName:'千度',
+    city:'上海',
+    industry:'互联网',
+    time:'20165-06-01 11:05'
+  },{
+    name:'WEB前端',
+    imgSrc:'image/company-1.png',
+    companyName:'慕课网',
+    city:'北京',
+    industry:'互联网',
+    time:'20165-06-01 01:05'
+  }];
 }])
