8e8e276f6bfc0c522c582badc0d3858a7613de19
ROG\zhyang <zhenghe_yang@hotmail.com>
2017-09-23 12:56:03 +0800

    增加职位和公司路由

--- src/scripts/config/router.js
+++ src/scripts/config/router.js
@@ -8,6 +8,17 @@ function ($stateProvider,$urlRouterProvider) {
         url:'/main',
         templateUrl:'view/main.html',
         controller:'mainCtrl'
-      });
+      })
+      .state('position',{
+        url:'/position/:id',
+        templateUrl:'view/position.html',
+        controller:'positionCtrl'
+      })
+      .state('company',{
+        url:'/company/:id',
+        templateUrl:'view/company.html',
+        controller:'companyCtrl'
+      })
+      ;
       $urlRouterProvider.otherwise('main')
 }])
